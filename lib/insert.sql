e4c027e10432b83966ef380f5fef138ebaf579f0
Hillary Muller <hillaryaward@gmail.com>
2022-08-22 15:45:00 -0500

    commit tree 33478621e4fe5b38bf0dfd675fb4421da442b00f

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "M", "brown", "sweet", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 1, "M", "black", "active", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 5, "F", "tan", "sleepy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 6, "M", "red", "hyper", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 3, "F", "brown", "sneezy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 4, "M", "white", "fun", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 4, "F", "white", "fun", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 4, "M", "white", "fun", 1);
